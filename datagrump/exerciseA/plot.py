e8c9783571550fac847151f0264bec86948bb092
jwvflauzino <jose.wilson.vf@gmail.com>
2018-11-01 15:52:11 -0200

    Atualizando exercicioA

--- datagrump/exerciseA/plot.py
+++ datagrump/exerciseA/plot.py
@@ -1,61 +1,123 @@
 import os
 import sys
 import matplotlib.pyplot as plot
+from matplotlib import colors as mcolors
 
 def make_plot(data):
-	x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 140, 160, 180, 200, 220, 240, 280, 300, 350]
+	#x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120, 140, 160, 180, 200, 220, 240, 280, 300, 350]
+	x = [1]
 	fig = plot.figure(figsize=(15,5))
 	axes = fig.add_subplot(111)
-	axes.bar(x, data, width=1, edgecolor='White', alpha=0.8, color='b')
-	axes.set_xlabel("Window Size")
-	plot.axis((0,360,0,0.015))
-	axes.set_ylabel("Potency")
-	plot.savefig("exerciseA/plot.png")
+	axes.bar(x, data, width=1, edgecolor='White', alpha=0.8, color='#086A87')
+	axes.set_xlabel("Tamanho da Janela de Congestionamento")
+	plot.axis((0,360,0,14.0))
+	axes.set_ylabel("Potencia (Taxa de Transferencia / Atraso")
+	plot.savefig("plot.png")
 
 def make_plot_zoom(data_zoom):
-        x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 40]
+	#x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 40]
+	x = [1]	
 	fig = plot.figure(figsize=(15,5))
-        axes = fig.add_subplot(111)
-        axes.bar(x, data_zoom, width=0.8, edgecolor='White', alpha=0.6, color='b')
-        axes.set_xlabel("Window Size")
-        plot.axis((0,35,0,0.015))
-        axes.set_ylabel("Potency")
-        plot.savefig("exerciseA/plot_zoom.png")
+	
+	axes = fig.add_subplot(111)
+	axes.bar(x, data_zoom, width=0.8, edgecolor='White', alpha=0.6, color='#086A87')
+	for i in range(len(x)):
+		axes.text(x[i] + 0.0, data_zoom[i] + 0.2, "{:.2f}".format(data_zoom[i]), fontsize=7, color='#0B3861')
+	
+	axes.set_xlabel("Tamanho da Janela de Congestionamento")
+	plot.axis((0,35,0,14.0))
+	axes.set_ylabel("Potencia (Taxa de Transferencia / Atraso)")
+	plot.savefig("plot_zoom.png")
+
+def make_plot_best(data_best):
+	x = [10, 11, 12, 13, 14, 15]
+	print "data_best: %s" % data_best
+	fig = plot.figure(figsize=(15,5))
+	
+	axes = fig.add_subplot(111)
+	axes.bar(x, data_best, width=0.8, edgecolor='White', alpha=0.6, color='#086A87')
+	for i in range(len(x)):
+		axes.text(x[i] + 0.25, data_best[i] - 0.1, "{:.2f}".format(data_best[i]), fontsize=16, color='w')
+	
+	axes.set_xlabel("Tamanho da Janela de Congestionamento")
+	plot.axis((9,16,12,13.0))
+	axes.set_ylabel("Potencia Mediana")
+	plot.savefig("plot_best.png")
 
 def read_all_file():
 	data = []
-	f = open('exerciseA/output.txt', 'r')
-	#f = open('output.txt', 'r')
+	f = open('output.txt', 'r')
 	texto = f.readlines()
 	for linha in texto :
 	    teste = linha.split(" ")
-	    p = round((float(teste[0]) / float(teste[1])),4)
+	    p = round((float(teste[0]) / float(teste[1]))*1000,4)
 	    potency = float("{0:.4f}".format(p))
 	    data.append(potency)
 	f.close()
 	return data
 
 def read_zoom_file():
-        data = []
-        f = open('exerciseA/output.txt', 'r')
-        #f = open('output.txt', 'r')
-        text = f.readlines()
+	data = []
+	f = open('output.txt', 'r')
+	text = f.readlines()
 	i = 1
-        for line in text :
-            if i <= 22:
-	    	part = line.split(" ")
-            	p = round((float(part[0]) / float(part[1])),4)
-            	potency = float("{0:.4f}".format(p))
-            	data.append(potency)
-	    i = i+1
-        f.close()
-        return data
+	for line in text :
+		#if i <= 22:
+		if i <=1:
+			part = line.split(" ")
+			p = round((float(part[0]) / float(part[1]))*1000,4)
+			potency = float("{0:.4f}".format(p))
+			data.append(potency)
+		i = i+1
+	f.close()
+	return data
+
+def read_best_file():
+	data = []
+	for j in range(1,11):
+		file_best = 'best/best%s.txt' % j
+		batery = []
+		#print "file: %s" % file_best
+		f = open(file_best, 'r')
+		text = f.readlines()
+		for line in text:
+			part = line.split(" ")
+			p = round((float(part[0]) / float(part[1]))*1000,4)
+			potency = float("{0:.4f}".format(p))
+			batery.append(potency)
+		data.append(batery)
+		f.close()
+	return data
+
+def show(data):
+	for i in data:
+		print i
+	print ""
+
+def print_best(data):
+	aux = []
+	sorted_aux = []
+	final_data = []	
+	wnd=0
+	for wnd in range(6):
+		for line in data:	
+			aux.append(line[wnd])
+		sorted_aux = sorted(aux)
+		final_data.append(sorted_aux[4])
+		del aux[:]
+		del sorted_aux[:]
+	return final_data
 
 def main():
-	data = read_all_file()
-	data_zoom = read_zoom_file()
-	make_plot(data)
-	make_plot_zoom(data_zoom)
+	#data = read_all_file()
+	#data_zoom = read_zoom_file()
+	data_best = read_best_file()
+	show(data_best)
+	data_best = print_best(data_best)
+	print "bests: %s" % data_best
+	make_plot_best(data_best)
+	#make_plot(data)
+	#make_plot_zoom(data_zoom)
 
 if __name__ == "__main__":
 	main()
--- datagrump/exerciseA/run.sh
+++ datagrump/exerciseA/run.sh
@@ -3,12 +3,17 @@
 # Exercise A script
 ###################
 echo "Running Exercise A"
+
+#------------------
+# Testing various window sizes
+#------------------
 sudo sysctl -w net.ipv4.ip_forward=1
 if [ -e "output.txt" ] ; then
-rm output.txt
+	rm output.txt
 fi
 touch output.txt
-array=('1' '2' '3' '4' '5' '6' '7' '8' '9' '10' '11' '12' '13' '14' '15' '16' '17' '18' '19' '20' '30' '40' '50' '60' '70' '80' '90' '100' '120' '140' '160' '180' '200' '220' '240' '280' '300' '350')
+#array=('1' '2' '3' '4' '5' '6' '7' '8' '9' '10' '11' '12' '13' '14' '15' '16' '17' '18' '19' '20' '30' '40' '50' '60' '70' '80' '90' '100' '120' '140' '160' '180' '200' '220' '240' '280' '300' '350')
+array=('1')
 cd ..
 if [ -e "controller.cc" ] ; then
 rm controller.cc
@@ -18,17 +23,64 @@ for i in ${array[@]}
 do
 	echo "Running with window size = "$i
 	sed -i "s/unsigned int the_window_size =.*/unsigned int the_window_size = "$i";/" controller.cc
-        cd ..
-        make
-        cd datagrump
-        ./run-contest jose_wilson &> "exerciseA/tmp.txt"
-        throughput=$(cat "exerciseA/tmp.txt" | grep "Average throughput:" | cut -d: -f2 | cut -d " " -f2)
+    cd ..
+    make
+    cd datagrump
+    ./run-contest jose_wilson &> "exerciseA/tmp.txt"
+    throughput=$(cat "exerciseA/tmp.txt" | grep "Average throughput:" | cut -d: -f2 | cut -d " " -f2)
 	echo "Throughput: " $throughput
 	delay=$(cat  "exerciseA/tmp.txt" | grep "95th percentile signal delay:" | cut -d: -f2 | cut -d " " -f2)
 	echo "Delay: " $delay
-	echo $throughput $delay >> 'exerciseA/output.txt'
+	echo $throughput $delay >> "exerciseA/output.txt"
 	rm "exerciseA/tmp.txt"
 done
-echo "Making plot"
-python exerciseA/plot.py
+cd exerciseA
+
+#------------------
+# Removing best.txt files
+#------------------
+for a in {1..30}
+do
+	if [ -e "best/best$a.txt" ] ; then	
+		rm best/best$a.txt
+	fi
+done
+
+
+#------------------
+# Running the best window size values
+#------------------
+best_array=('10' '11' '12' '13' '14' '15')
+#best_array=('10' '11')
+for x in {1..10}
+do
+	if [ "$x" -ne "1" ] ; then
+		cd exerciseA
+	fi
+	touch best/best$x.txt
+	for j in ${best_array[@]}
+	do
+		echo
+		echo "---- BEST ----"
+		echo "Test$x: Running with window size = "$j
+		if [ "$j" -eq "10" ] ; then
+			cd ..
+		fi
+		sed -i "s/unsigned int the_window_size =.*/unsigned int the_window_size = "$j";/" controller.cc
+		cd ..
+		make
+		cd datagrump
+		./run-contest jose_wilson &> "exerciseA/tmp.txt"
+		throughput=$(cat "exerciseA/tmp.txt" | grep "Average throughput:" | cut -d: -f2 | cut -d " " -f2)
+		echo "Throughput: " $throughput
+		delay=$(cat  "exerciseA/tmp.txt" | grep "95th percentile signal delay:" | cut -d: -f2 | cut -d " " -f2)
+		echo "Delay: " $delay
+		echo $throughput $delay >> "exerciseA/best/best$x.txt"
+		rm "exerciseA/tmp.txt"
+	done
+done
+cd exerciseA
+
+echo "Making plots"
+python plot.py
 echo "Finish"
